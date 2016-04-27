388dbe5656e484b08c18a6efc21128782a16390c
extiser <1sappho@gmail.com>
2016-04-27 14:31:32 +0500

    partial of pages

---
+++ markup/modules/filter/filter.js
@@ -0,0 +1,4 @@
+$('.checkbox').on('click',function(e) {
+  e.preventDefault();
+  $(this).toggleClass('active');
+});
---
+++ markup/modules/last-news/last-news.js
@@ -0,0 +1,8 @@
+$('.last-news-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  centerMode: false,
+  focusOnSelect: true,
+  arrows: false
+});
