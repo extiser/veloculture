a1ddb4de330e3a417b7c0132333e2cbce88c3050
extiser <1sappho@gmail.com>
2016-04-29 13:46:30 +0500

    basket pop & compare pop

--- markup/modules/topbar/topbar.js
+++ markup/modules/topbar/topbar.js
@@ -1,1 +1,9 @@
+$('.top-bar-compare').click(function (e) {
+  e.preventDefault();
+  $('.top-bar-compare-pop').slideToggle(400);
+});
 
+$('.top-bar-basket').click(function (e) {
+  e.preventDefault();
+  $('.top-bar-basket-pop').slideToggle(400);
+});
